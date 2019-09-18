904c461c8b900d510738c5a112fc48b091dc3fd3
wangyaqi <wangyaqi.chn@foxmail.com>
2019-09-18 17:16:13 +0800

    init: basic feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React from 'react'
+import styles from './index.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <textarea></textarea>
-      <div></div>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.$vm = React.createRef()
+    this.state = {
+      content: '',
+      renderContent: [],
+      input: ''
+    }
+  }
+
+  contentClick() {
+    this.$vm.current.focus()
+  }
+
+  changeHandler(e) {
+    const content = e.target.value
+    const arr = content.split('\n')
+    this.setState({
+      content,
+      renderContent: arr
+    })
+  }
+
+  render() {
+    const { content, renderContent } = this.state
+
+    return (
+      <div className="App">
+        <textarea
+          ref={this.$vm}
+          onChange={e => this.changeHandler(e)}
+        ></textarea>
+        <div className={styles.content} onClick={() => this.contentClick()}>
+          {renderContent.map((item, index) => (
+            <div key={index}>
+              {item ? <span>{item}</span> : <span>&#8203;</span>}
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
